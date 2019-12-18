08e98b378c0d648ac3dbfd8b9aac16d7313ea6de
alexblokstra <alexblokstra@live.nl>
2019-12-18 14:54:39 +0100

    Test

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,10 @@ Route::get('', function () {
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::view('/welkom', 'welkom');
-<<<<<<< HEAD
-Route::get('/home', 'DogsController@index');
 
-Route::get('/home', 'DogsController@index');
-=======
 Route::get('/home', 'HomeController@index');
 Route::get('/honden', 'DogsController@index');
->>>>>>> aebd1510057b44ba6e79f1d5c5876a7790e61cc4
+
 Route::get('/honden', 'DogsController@create');
 Route::post('/honden', 'DogsController@store');
 Route::get('/honden/{dog}', 'DogsController@show');
