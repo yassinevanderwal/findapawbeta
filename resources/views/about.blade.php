cf23c309c6889a2c43ea699399641a01e5b97d7c
yassinevanderwal <yassinvanderwal@hotmail.com>
2019-12-19 13:48:45 +0100

    removed

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layout')
-
-@section('content')
-<h1> Informatie over ons </h1>
-<h3> Informatie over het bedrijf. </h3>
-@endsection
--- resources/views/contact.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')
-
-@section('content')
-<h1> Contact </h1>
-@endsection
